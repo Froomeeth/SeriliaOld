efa22f8db1a7a59744ea6bfce0728c93451ffd68
nullevoy <89076920+stacktrace-error@users.noreply.github.com>
2023-08-13 02:18:52 +0200

    godfuckingdamnit

--- src/serilia/world/draw/drawblock/DrawConstructUniversal.java
+++ src/serilia/world/draw/drawblock/DrawConstructUniversal.java
@@ -19,21 +19,21 @@ public class DrawConstructUniversal extends DrawBlock{
     @Override
     public void draw(Building build){
         Draw.draw(Layer.blockOver, () -> {
-            if(reconstruct){
+            if(reconstruct && fromContent(build) != null){
                 Draw.alpha(1f - build.progress());
-                Draw.rect(fromContent((UniversalBuild)build).fullIcon, build.x, build.y, ((UniversalBuild)build).payload instanceof UnitPayload ? build.rotation() - 90 : 0f);
+                Draw.rect(fromContent(build).fullIcon, build.x, build.y, ((UniversalBuild)build).payload instanceof UnitPayload ? build.rotation() - 90 : 0f);
                 Draw.reset();
             }
-            Drawf.construct(build, toContent((UniversalBuild) build), build.rotdeg() - 90f, build.progress(), build.warmup(), build.totalProgress());
+            if(toContent(build) != null) Drawf.construct(build, toContent(build), build.rotdeg() - 90f, build.progress(), build.warmup(), build.totalProgress());
         });
     }
 
     /**Override in the "new DrawConstructUniversal(){{}}" if you want something other than payloads.
      * I know you have no idea how, so just ask me or check an example if you want that.*/
-    public UnlockableContent fromContent(UniversalBuild build){
-        return build.lastPayload;
+    public UnlockableContent fromContent(Building build){
+        return ((UniversalBuild)build).lastPayload != null && ((UniversalBuild)build).mmmDelish.get(((UniversalBuild)build).lastPayload) > 0 ? ((UniversalBuild)build).lastPayload : null;
     }
-    public UnlockableContent toContent(UniversalBuild build){
-        return build.currentRecipe.payOut.get(0).item;
+    public UnlockableContent toContent(Building build){
+        return ((UniversalBuild)build).currentRecipe.payOut.get(0).item;
     }
 }
